733e144474a877a8ba449e547a3aad49f8f7dad8
Gaston Acosta <gasteac@gmail.com>
2024-05-31 12:38:44 -0300

    Improve image loading in DashProfile component

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -317,12 +317,14 @@ export const DashProfile = () => {
                 }}
               />
             )}
+            <div className="tooltip tooltip-open tooltip-bottom" data-tip="Click to change">
             <img
               //Si el usuario subió una imagen la muestro, sino muestro la imagen de perfil del usuario
               src={imageFileUrl ? imageFileUrl : currentUser.profilePic}
               alt="user"
               className="rounded-full w-full h-full  object-cover border-[lightgray] "
             />
+            </div>
           </div>
         </div>
 
